db4e154d3d892dfd493376fcf4f12aa67be4e55c
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2018-02-03 13:07:05 -0800

    Works

--- SwiftMetaCoder/ViewController.swift
+++ SwiftMetaCoder/ViewController.swift
@@ -31,14 +31,36 @@ class ViewController: NSViewController {
 
 
     @IBAction func userPressedDone(_ sender: NSButton) {
-        print("Input Field: \(inputTextField.stringValue)")
-        print("Template Field: \(templateTextField.stringValue)")
         let environment = Environment()
 
-        let context = ["name": "Shaheen"]
-        let output = try? environment.renderTemplate(string: "Hello {{ name }}", context: context)
-        print("Output Field: \(output ?? "fail")")
+        if let json = inputTextField.stringValue.json {
+            if let context = json as? [String: Any] {
+                if let output = try? environment.renderTemplate(string: templateTextField.stringValue, context: context) {
+                    outputTextField.stringValue = output
+                } else {
+                    outputTextField.stringValue = "Couldn't render the template against the provided JSON"
+                }
+            } else {
+                outputTextField.stringValue = "Couldn't cast JSON to [String:Any] Swift type"
+            }
+        } else {
+            outputTextField.stringValue = "Couldn't parse JSON input"
+        }
     }
-
 }
 
+extension String {
+
+    var json: AnyObject? {
+
+        let data = self.data(using: String.Encoding.utf8, allowLossyConversion: false)
+
+        if let jsonData = data {
+            // Will return an object or nil if JSON decoding fails
+            return try? JSONSerialization.jsonObject(with: jsonData, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+        } else {
+            // Lossless conversion of the string was not possible
+            return nil
+        }
+    }
+}
